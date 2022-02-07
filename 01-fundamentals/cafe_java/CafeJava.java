e1907e13d7a276a2cffeaa997a3ed58f9030db2b
Nick Smart <nsmart147@gmail.com>
2022-02-07 12:19:11 -0500

    cafe java complete

--- 01-fundamentals/cafe_java/CafeJava.java
+++ 01-fundamentals/cafe_java/CafeJava.java
@@ -29,8 +29,11 @@ public class CafeJava {
         // Example:
         System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
         // ** Your customer interaction print statements will go here ** //
+        //1. Cindhurri ordered coffee, show status
         System.out.println(customer1 + pendingMessage);
 
+        //2. Noah ordered cappuccino
+        // check order status and respond
         if (isReadyOrder4) {
             System.out.println(customer4 + readyMessage);
             System.out.println(displayTotalMessage + cappuccinoPrice);
@@ -38,11 +41,17 @@ public class CafeJava {
         } else {
             System.out.println(customer4 + pendingMessage);
         }
+
+        //3. Sam ordered 2 lattes, tell price and order status
         System.out.println(generalGreeting + customer2);
-        if (isReadyOrder2){
-            System.out.println(displayTotalMessage + lattePrice * 2);
+        System.out.println(displayTotalMessage + lattePrice * 2);
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        } else {
+            System.out.println(customer2 + pendingMessage);
         }
 
+        //4. Jimmy was charged for coffee but ordered latte, tem him new total
         System.out.println(customer3 + readyMessage);
         System.out.println(displayTotalMessage + (lattePrice - dripCoffeePrice));
 

dbdbf2925148100334d63d47081d6c52b274a637
Nick Smart <nsmart147@gmail.com>
2022-02-09 10:25:39 -0500

    added comments version

--- 00-algos/removeDupes.js
+++ 00-algos/removeDupes.js
@@ -21,3 +21,27 @@ const removeDupes = (arr) => {
 arr = [3,6,7,3,2,7,7,9,0,1];
 removeDupes(arr);
 console.log(arr);
+
+
+const removeDupes = (arr) => {
+    let arrObj = {};
+
+    for(let i = 0; i < arr.length; i++){
+        // if value at index i not in array object, then add it to array object with value of 1
+        if (!(arr[i] in arrObj)){
+            arrObj[arr[i]] = 1;
+        }
+        else if (arr[i] in arrObj){
+            // otherwise , if value at index i is in array object, then remove it from array using splice.
+            // splice takes in an index (i) and a number of elements to remove (only 1 in this case)
+            arr.splice(i, 1)
+            // because splice removes the value at index i and shifts the array, we need to decrement i
+            // to avoid skipping any values
+            i--;
+        }
+    }
+}
+
+arr = [3,6,7,3,2,7,7,9,0,1];
+removeDupes(arr);
+console.log(arr);

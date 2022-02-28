00a2cfdaea98868ba63aab83ce23d4f543e9ec2c
Nick Smart <nsmart147@gmail.com>
2022-02-28 11:00:44 -0500

    cleaning/refactoring

--- 04-javaMVC/EXAM/src/main/java/com/smart/javabeltexam/ServletInitializer.java
+++ 04-javaMVC/EXAM/src/main/java/com/smart/javabeltexam/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(LoginRegistrationApplication.class);
+		return application.sources(JavaBeltExamApplication.class);
 	}
 
 }
